0b8bedea0ea16334eed42faefe071198631286db
Aaron Santos <aaron.santos-ext@viavarejo.com.br>
2020-12-07 10:25:00 -0300

    feat: add useless exclamation point in button

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,8 +6,7 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-knobs/register"
+    "@storybook/preset-create-react-app"
   ],
   "viewMode": "docs",
 }
